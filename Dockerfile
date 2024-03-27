b4bea000835becb274d5b9241c5f4e64b94f60ec
leawn <leonrubner2809@gmail.com>
2024-03-27 19:15:04 +0100

    Add dag that runs mlflow script

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,7 @@ RUN mkdir -p dags && \
 COPY --chown=airflow:root dags/ /opt/airflow/dags
 COPY --chown=airflow:root config/ /opt/airflow/config
 COPY --chown=airflow:root plugins/ /opt/airflow/plugins
+COPY --chown=airflow:root mlflow/ /opt/airflow/mlflow
 
 COPY requirements.txt /
 RUN pip install --no-cache-dir "apache-airflow==${AIRFLOW_VERSION}" -r /requirements.txt
---
+++ dags/onboarding.py
@@ -0,0 +1,32 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'email': ['restack+publisher@restack.io'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'mlflow_train_model',
+    default_args=default_args,
+    description='A simple DAG to run MLflow training script',
+    schedule_interval=timedelta(days=1),
+)
+
+# Make sure to use the absolute path to your train.py script
+train_model_task = BashOperator(
+    task_id='train_model',
+    bash_command='python /path/to/your/train.py',
+    env={'MLFLOW_TRACKING_URI': 'https://mlt6ur.clj5khk.gcp.restack.it', 'MLFLOW_TRACKING_USERNAME': 'Restack', 'MLFLOW_TRACKING_PASSWORD': 'd3stg6v3xb'},  # Set your MLflow tracking URI
+    dag=dag,
+)
+
+train_model_task
