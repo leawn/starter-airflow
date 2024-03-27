459ae41064b09cf9fd0e1f9e91e500103b04a657
leawn <leonrubner2809@gmail.com>
2024-03-27 18:27:49 +0100

    Add train.py

---
+++ train.py
@@ -0,0 +1,81 @@
+import os
+import mlflow
+from mlflow.models import infer_signature
+
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# Set the tracking URI for MLflow
+mlflow.set_tracking_uri('https://mlt6ur.clj5khk.gcp.restack.it')
+
+# Set the credentials for the MLflow tracking server
+os.environ['MLFLOW_TRACKING_USERNAME'] = 'Restack'
+os.environ['MLFLOW_TRACKING_PASSWORD'] = 'd3stg6v3xb'
+
+
+# Load the Iris dataset
+X, y = datasets.load_iris(return_X_y=True)
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Define the model hyperparameters
+params = {
+    "solver": "lbfgs",
+    "max_iter": 1000,
+    "multi_class": "auto",
+    "random_state": 8888,
+}
+
+# Train the model
+lr = LogisticRegression(**params)
+lr.fit(X_train, y_train)
+
+# Predict on the test set
+y_pred = lr.predict(X_test)
+
+# Calculate metrics
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred, average='macro')
+recall = recall_score(y_test, y_pred, average='macro')
+f1 = f1_score(y_test, y_pred, average='macro')
+
+
+mlflow.set_experiment("Mlflow Quickstart")
+
+with mlflow.start_run():
+    mlflow.log_params(params)
+
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("precision", precision)
+    mlflow.log_metric("recall", recall)
+    mlflow.log_metric("f1", f1)
+
+    mlflow.set_tag("Training Info", "Basic LR model for iris data")
+
+    signature = infer_signature(X_train, lr.predict(X_train))
+
+    model_info = mlflow.sklearn.log_model(
+        sk_model=lr,
+        artifact_path="iris_model",
+        signature=signature,
+        input_example=X_train,
+        registered_model_name="tracking-quickstart",
+    )
+
+loaded_model = mlflow.pyfunc.load_model(model_info.model_uri)
+
+predictions = loaded_model.predict(X_test)
+
+iris_feature_names = datasets.load_iris().feature_names
+
+result = pd.DataFrame(X_test, columns=iris_feature_names)
+result["actual_class"] = y_test
+result["predicted_class"] = predictions
+
+result[:4]
